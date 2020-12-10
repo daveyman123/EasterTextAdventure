157f6f1dcb68efd4b1e1735c277464d1226d1ee6
Bob <dgraff8@my.stlcc.edu>
2020-12-09 22:41:24 -0600

    made changes to rabbit scene

--- EasterTextAdventure/Scenes/RabbitStreet.cs
+++ EasterTextAdventure/Scenes/RabbitStreet.cs
@@ -9,16 +9,35 @@ namespace EasterTextAdventure.Scenes
     {
        string RSsceneArt = Art.Art.Bunny();
        string RSaddText = "You are at Rabbit Street. Vinnie, the rabbit, looks busy.";
-       string[] RSoptions = new String[] { "Talk to vinnie", "Giovanni's Garden", "Inventory","Exit Game" };
+       string[] RSoptions = new String[] { "Talk to Vinnie the rabbit", "Giovanni's Garden", "Inventory","Exit Game" };
 
 
     public RabbitStreet (Game game) : base(game)
+        {
+            Initialize(RSsceneArt,RSaddText,RSoptions);
+        }
+        private void Initialize(
+              string RSsceneArt, string RSaddText, string[] RSoptions)
         {
             sceneArt = RSsceneArt;
             addText = RSaddText;
             options = RSoptions;
         }
+        //resets the original variables to when the class was initialized
+        private void Reset()
 
+        {
+            Initialize(RSsceneArt, RSaddText, RSoptions);
+        }
+
+        //talk to vinnie initial part
+        private void TalkVinnie()
+        {
+            addText = "Welcome to Rabbit Street Kid, Wadda ya want? wadda wadda-wad da ya wan? get it? I am the King of rabbit streety street, but you can be too!!!";
+            sceneArt = Art.Art.Bunny2();
+            options = new String[] { "Whats Rabbit Street?", "How do I become the king of Rabbit Street?", "Who is actually the king of Rabbit Street?", "Leave Vinnie Alone" };
+
+        }
         public override void Run()
         {
           
@@ -27,11 +46,12 @@ namespace EasterTextAdventure.Scenes
             {
                 case 1:
                     //talk to vinnie
+                    TalkVinnie();
+
 
                     //vinnies options
-                    addText = "Welcome to Rabbit Street Kid, Wadda ya want? wadda wadda-wad da ya wan? get it? I am the King of rabbit streety street, but you can be too!!!";
-                    sceneArt = Art.Art.Bunny2();
-                    options = new String[] { "Whats Rabbit Street?", "How do I become the king of Rabbit Street?", "Who is actually the king of Rabbit Street?", "Leave Vinnie Alone" };
+
+
                     base.Run();
                     switch (optionsChoice)
                     {
@@ -43,7 +63,19 @@ namespace EasterTextAdventure.Scenes
                         
                             addText = "You kids never understand. let me get back to sorting, or Giovanni's gonna whip my ass!";
 
-                            options = new string[] { "OK, just one more question though."};
+                            options = new string[] { "OK, just one more question though.", "I'll let you get back to whatever you were doing."};
+                            base.Run();
+                            switch (optionsChoice)
+                            {
+                                case 1:
+                                    TalkVinnie();
+                                    
+                                    break;
+                                case 2:
+                                    Reset();
+                                    
+                                    break;
+                            }
 
 
 
@@ -51,10 +83,8 @@ namespace EasterTextAdventure.Scenes
 
 
 
-                            
-
-                            
                             myGame.myRabbitStreetScene.Run();
+                            
                             return;
                         case 2:
                             addText = "You wouldnt believe the things Giovanni put us through in order that he could earn his precious title of 'King' of Rabbit Street. But why are you asking me? Everyone knows I'm not a good source of info. Thats why they call me stupid vinney. Honestly, take a bit of rabbit advice and get out of rabbit street while you still can. else you'll end up like me pilfering through baskets of leftover easter baskets looking for anything salvagable.....";
@@ -74,18 +104,13 @@ namespace EasterTextAdventure.Scenes
                                     addText = "Giovanni killed all the rival rabbit gang members. least thats what i hear. but I dont hear to good ya'hear?";
                                     options = new string[] { "Leave Vinnie alone" };
                                     base.Run();
-                                    //sceneArt = RSsceneArt;
-                                    //addText = RSaddText;
-                                    //options = RSoptions;
-                                    //myGame.myRabbitStreetScene.Run();
+                       
 
 
                                     addText = "alright kid i like you a little and i will be there to help you out.";
                                     options = new string[] { "Ok, whatever, Vinnie" };
                                     base.Run();
-                                    sceneArt = RSsceneArt;
-                                    addText = RSaddText;
-                                    options = RSoptions;
+                                    Reset();
                                     myGame.myRabbitStreetScene.Run();
 
                                     return;
@@ -93,17 +118,13 @@ namespace EasterTextAdventure.Scenes
                                     addText = "alright kid i like you a little and i will be there to help you out.";
                                     options = new string[] { "Ok, whatever, Vinnie" };
                                     base.Run();
-                                    sceneArt = RSsceneArt;
-                                    addText = RSaddText;
-                                    options = RSoptions;
+                                    Reset();
                                     myGame.myRabbitStreetScene.Run();
                                     return;
                             }
                             return;
                                 case 4:
-                            sceneArt = RSsceneArt;
-                            addText = RSaddText;
-                            options = RSoptions;
+                            Reset();
                             myGame.myRabbitStreetScene.Run();
                             return;
 
